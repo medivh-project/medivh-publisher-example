b0001357a55d514ea1bf11f8eca8f47747124cc8
Xuan-Zhang Gong <gongxuanzhangmelt@gmail.com>
2024-10-14 17:56:50 +0800

    init

---
+++ src/main/kotlin/tech/medivh/publisher/example/MedivhExampleDemo.kt
@@ -0,0 +1,11 @@
+package tech.medivh.publisher.example
+
+
+/**
+ * @author gxz gongxuanzhangmelt@gmail.com
+ **/
+class MedivhExampleDemo {
+    fun helloWorld() {
+        println("Hello World")
+    }
+}
